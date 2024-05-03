3da80fcae10addb38d94dfe61ed5ce59c127a337
Ashraf Ali <ashrafali786522002@gmail.com>
2024-05-03 11:36:22 +0530

    update all routes and component , constants, api

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default Header
---
+++ src/context/contextApi.js
@@ -0,0 +1,44 @@
+import React, { createContext, useEffect, useState } from "react";
+import {fetchDataFromApi} from "../utils/api"
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState(false);
+  const [selectCategories, setSelectCategories] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectCategoryData(selectCategories)
+  }, [selectCategories]);
+
+  const fetchSelectCategoryData = (query) => {
+    setLoading(true)
+    fetchDataFromApi(`search/?q=$(query)`).then(({contents}) => {
+        console.log(contents);
+        setSearchResults(contents)
+        setLoading(false);
+    }).catch((err) => {
+        
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+          loading,
+          setLoading,
+          searchResults,
+          setSearchResults,
+          selectCategories,
+          setSelectCategories,
+          mobileMenu,
+          setMobileMenu
+
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  )
+};
